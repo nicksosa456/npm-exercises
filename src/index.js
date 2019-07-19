2ad2ee72c7ccaff3ab38a488327f4b723844455d
Nicholas Sosa <nicksosa456@gmail.com>
2019-07-19 13:53:23 -0500

    modules exercise

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-const sayHell = () => {
-    console.log('hello');
-};
-sayHell();
+// const sayHello = () => {
+//     console.log('hello');
+// };
+// sayHello();
 
 const $ = require('jquery');
+// const pokemon = require('pokemon');
+// const pokemonGif = require('pokemon-gif');
+// const randomPoke = pokemon.random();
+// console.log(randomPoke);
+// const randomGif = pokemonGif(randomPoke);
+// $('#poke').html('<img src="'+randomGif+'">');
 
-$('body').css('background', 'red');
+// const hi = require('./say-hello.js');
+// import hi from './say-hello.js';
+import {hello, add, subtract, multiply, divide} from "./say-hello";
+
+// console.log(hi.hello());
+
+// console.log(hi.multiply(37,620));
+
+console.log(hello());
+console.log(divide(340, 50));
+console.log(multiply(123,7560));
+console.log(add(3564.12, 756.091));
---
+++ src/say-hello.js
@@ -0,0 +1,9 @@
+export const hello = () => 'Hey';
+export const add = (x, y) => x + y;
+export const subtract = (x, y) => x - y;
+export const multiply = (x, y) => x * y;
+export const divide = (x, y) => x / y;
+
+// module.exports = {hello, add, subtract, multiply, divide};
+
+// export default {hello, add, subtract, multiply, divide};
